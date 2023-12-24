f75d873c7b8e9cd75dc47cae519c4c3f38d1bf6c
adammetzinger <adamcm728@gmail.com>
2023-12-24 15:34:25 -0600

    added draggable boats

--- mainjs/main.js
+++ mainjs/main.js
@@ -1,5 +1,5 @@
 const ships = document.querySelectorAll('#boat5');
-
+const board = document.querySelectorAll('#playerboard > div');
 
 ships.forEach(ship => {
     ship.addEventListener('dragstart', () => {
@@ -9,4 +9,11 @@ ships.forEach(ship => {
     ship.addEventListener('dragend', () => {
         ship.classList.remove('dragging')
     })
+})
+
+board.forEach(div  => {
+   div.addEventListener('dragover', () => {
+    const draggable = document.querySelector('.dragging')
+    div.appendChild(draggable)
+   })
 })
